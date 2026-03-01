b4866c8208578f182d10d4592d9b656535a25a7b
Fernando José da Silveira <fernanojsilveira@gmail.com>
2026-03-01 14:38:32 -0300

    Ajuste do Util Pmoc para funcionar no Page

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true'
+const repositoryParts = process.env.GITHUB_REPOSITORY?.split('/')
+const repositoryOwner = repositoryParts?.[0]
+const repositoryName = repositoryParts?.[1]
+const isUserOrOrgSite = !!repositoryOwner && repositoryName === `${repositoryOwner}.github.io`
+const basePath = isGithubActions && repositoryName && !isUserOrOrgSite ? `/${repositoryName}` : ''
+
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  basePath,
+  assetPrefix: basePath,
   typescript: {
     ignoreBuildErrors: true,
   },
